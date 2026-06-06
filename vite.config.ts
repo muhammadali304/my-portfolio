ff89d2d712ee8bdb26cc840b4db28a5126e6cd77
muhammadali304 <159603285+muhammadali304@users.noreply.github.com>
2026-06-07 00:52:12 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
+  plugins: [],
   tanstackStart: {
-    plugins: [],
     server: { entry: "server" },
   },
 });
